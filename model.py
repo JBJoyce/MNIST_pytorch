f893957d78c7894a41117624d1e4a4485162d2fc
Jeremiah B Joyce <jeremiah.b.joyce@gmail.com>
2024-03-14 12:35:47 -0500

    add model.py

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FirstModel(torch.nn.Module):
+    
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        x = nn.Flatten(x)
+        x = self.l1(x)
+        x = F.ReLU(x)
+        x = self.l2(x)
+        x = F.ReLU(x)
+        x = F.log_softmax(x)
+        return x
+    
+if __name__ == "__main__":
+    first_model = FirstModel(28*28, 50, 10)
+    print(first_model)
+        
+        
+        
+        
+        
