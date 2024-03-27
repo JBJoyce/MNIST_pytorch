93c142310713638c81ff2ec15d4f3d2d400bbe2a
Jeremiah Joyce <59209884+JBJoyce@users.noreply.github.com>
2024-03-27 12:35:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -54,12 +54,13 @@ def test_loop(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-for epoch in range(epochs):
-    print(f'Epoch {epoch+1}\n-------------------------------')
-    train_loop(train_dl, model, loss_fn, optimizer)
+if __name__ == '__main__':
+    for epoch in range(epochs):
+        print(f'Epoch {epoch+1}\n-------------------------------')
+        train_loop(train_dl, model, loss_fn, optimizer)
 
-print('Training done\n')
-test_loop(valid_dl, model, loss_fn)
-print('Validation done\n')
-test_loop(test_dl, model, loss_fn)
-print('Validation done')  
+    print('Training done\n')
+    test_loop(valid_dl, model, loss_fn)
+    print('Validation done\n')
+    test_loop(test_dl, model, loss_fn)
+    print('Validation done')  
