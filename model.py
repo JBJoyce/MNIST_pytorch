1e965007d9c533946a4cecf98c3081977b2ebf35
Jeremiah Joyce <59209884+JBJoyce@users.noreply.github.com>
2024-03-27 12:09:18 -0500

    Update model.py

--- model.py
+++ model.py
@@ -6,12 +6,13 @@ class FirstModel(torch.nn.Module):
     
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
+        self.flatten = nn.Flatten()
         self.l1 = nn.Linear(input_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, hidden_size)
         self.l3 = nn.Linear(hidden_size, output_size)
         
     def forward(self, x):
-        x = torch.Flatten(x)
+        x = self.Flatten(x)
         x = self.l1(x)
         x = F.ReLU(x)
         x = self.l2(x)
